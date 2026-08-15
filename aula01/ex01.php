b52c80de9c4b291b9006bf0f23025b161791a377
Matheus Lanzieri <matheus.lanzieri@outlook.com>
2026-08-15 14:14:13 -0300

    Atividade em sala de aula

---
+++ aula01/ex01.php
@@ -0,0 +1,8 @@
+<?php    
+$nota1 = 8;
+$nota2 = 10;
+$nota3 = 6;
+$media = ($nota1 + $nota2 + $nota3) /3;
+echo "A média de nota é: " . $media;
+
+?>
---
+++ aula01/ex03.php
@@ -0,0 +1,7 @@
+<?php    
+$bmaior = 20;
+$bmenor = 12;
+$altura = 15;
+$areatrap = (($bmaior + $bmenor)*$altura)/2 ;
+echo "A área do trapézio é: " . $areatrap;
+?>
